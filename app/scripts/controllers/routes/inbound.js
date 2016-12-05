990291e0cb2a11ba56a7e8832acc716434650770
Giovanni Bezicheri <giovanni.bezicheri@nethesis.it>
2016-12-05 11:25:14 +0100

    Add inbound route wizard step.

--- app/scripts/controllers/routes/inbound.js
+++ app/scripts/controllers/routes/inbound.js
@@ -8,10 +8,30 @@
  * Controller of the nethvoiceWizardUiApp
  */
 angular.module('nethvoiceWizardUiApp')
-  .controller('RoutesInboundCtrl', function() {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RoutesInboundCtrl', function($scope, RouteService) {
+    RouteService.inbounds().then(function (res) {
+      $scope.routes = res.data;
+      $scope.show = true;
+    });
+
+    // Remove a route
+    $scope.deleteRoute = function(did, cid) {
+      RouteService.deleteInboundRoute(did, cid).then(function (res) {
+        RouteService.inbounds().then(function (res) {
+          $scope.routes = res.data;
+          $scope.show = true;
+        });
+      });
+    };
+
+    // Modify a route with Visual Plan
+    $scope.modifyRoute = function(did, cid) {
+      window.open(customConfig.VPLAN_URL + '?id=' + encodeURI(did + ' / ' + cid), '_blank');
+    };
+
+    // Create a new route with Visual Plan
+    $scope.newRoute = function() {
+      window.open(customConfig.VPLAN_URL + '?did=new_route');
+    };
+
   });
--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -3,5 +3,6 @@ var customConfig = {
   BRAND_SITE: 'http://www.nethvoice.it',
   BRAND_DOCS: 'http://nethvoice.docs.nethesis.it/it/latest/',
   BASE_API_URL: '/freepbx/rest-dev',
+  VPLAN_URL: 'http://nser7.russo.net/freepbx/visualplan/index.html',
   SECRET_KEY: '1234'
 };
