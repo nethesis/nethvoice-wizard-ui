ea36b1d8954978c1719fa6103731ad77b8b61cac
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2016-12-13 15:08:34 +0100

    fix absolute path of backend resources

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -2,7 +2,7 @@ var customConfig = {
   BRAND_NAME: 'NethVoice',
   BRAND_SITE: 'http://www.nethvoice.it',
   BRAND_DOCS: 'http://nethvoice.docs.nethesis.it/it/latest/',
-  BASE_API_URL: 'freepbx/rest',
-  VPLAN_URL: 'freepbx/visualplan',
+  BASE_API_URL: '/freepbx/rest',
+  VPLAN_URL: '/freepbx/visualplan',
   SECRET_KEY: '1234'
 };
