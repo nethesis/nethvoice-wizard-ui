3b6f01215f3b08e96a203b9e97e6f34ef642c90f
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2016-12-20 16:50:47 +0100

    fix config url

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -2,7 +2,7 @@ var customConfig = {
   BRAND_NAME: 'NethVoice',
   BRAND_SITE: 'http://www.nethvoice.it',
   BRAND_DOCS: 'http://nethvoice.docs.nethesis.it/it/latest/',
-  BASE_API_URL: 'https://192.168.5.102/freepbx/rest-dev',
+  BASE_API_URL: '/freepbx/rest',
   VPLAN_URL: '/freepbx/visualplan',
   SECRET_KEY: '1234'
 };
