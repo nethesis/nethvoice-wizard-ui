568b1c913158e7b7e4c477d390b5174ea263a17d
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2017-05-15 09:44:52 +0200

    reset custom.js

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -2,9 +2,9 @@ var customConfig = {
   BRAND_NAME: 'NethVoice',
   BRAND_SITE: 'http://www.nethvoice.it',
   BRAND_DOCS: 'http://nethvoice.docs.nethesis.it/it/latest/',
-  BASE_API_URL: 'https://nethpanico.nethesis.it/freepbx/rest',
-  BASE_API_URL_CTI: 'https://nethpanico.nethesis.it/webrest',
-  VPLAN_URL: 'https://nethpanico.nethesis.it/freepbx/visualplan',
+  BASE_API_URL: '/freepbx/rest',
+  BASE_API_URL_CTI: '/webrest',
+  VPLAN_URL: '/freepbx/visualplan',
   OUTBOUNDS_URL: '/freepbx/admin/config.php?display=routing&view=form&id=',
-  SECRET_KEY: 'q_SPxqTuHHfYXKZE'
+  SECRET_KEY: '1234'
 };
