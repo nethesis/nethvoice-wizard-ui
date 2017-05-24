38adad7aca70055809316c6e0b22188796bc4cf5
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2017-05-24 16:34:23 +0200

    fixed template custom name for new template

--- app/scripts/controllers/apps/cards.js
+++ app/scripts/controllers/apps/cards.js
@@ -482,7 +482,7 @@ angular.module('nethvoiceWizardUiApp')
       g.isChecking = true;
       ApplicationService.customerCardPreview({
         dbconn_id: g.dbconn_id,
-        template: g.template,
+        template: g.template.custom ? g.template.name + '_custom' : g.template.name,
         query: btoa(g.query)
       }).then(function (res) {
         g.render_html = '<style>body{overflow: auto !important; padding: 5px !important;}</style><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.10/semantic.min.css"/>' + atob(res.data.html);
