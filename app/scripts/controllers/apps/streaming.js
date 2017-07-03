2c8ebbf202f0d17f3682f94a2aa2421dc4d8fb78
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2017-07-03 12:29:41 +0200

    added streaming and source video views

---
+++ app/scripts/controllers/apps/streaming.js
@@ -0,0 +1,119 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nethvoiceWizardUiApp.controller:AppsStreamingCtrl
+ * @description
+ * # AppsStreamingCtrl
+ * Controller of the nethvoiceWizardUiApp
+ */
+angular.module('nethvoiceWizardUiApp')
+  .controller('AppsStreamingCtrl', function ($scope, ProfileService, ApplicationService, UserService) {
+    $scope.allSources = [];
+    $scope.allProfiles = [];
+    $scope.newSource = {
+      verified: false,
+      isChecking: false,
+      checked: false,
+      showPass: false
+    };
+    $scope.currentSource = {};
+    $scope.users = [];
+
+    $scope.selectSource = function (s) {
+      $scope.currentSource = s;
+    };
+    $scope.saveSource = function (s) {
+      s.onSave = true;
+      // clean useless data
+      delete s.checked;
+      delete s.isChecking;
+      delete s.onSave;
+      delete s.verified;
+      ApplicationService.createVideoSource(s).then(function (res) {
+        s.onSave = false;
+        $scope.onSaveSuccessSource = true;
+        $scope.onSaveErrorSource = false;
+        $scope.allSources.push(s);
+        $scope.newSource = {
+          verified: false,
+          isChecking: false,
+          checked: false
+        };
+        $('#newSourceModal').modal('hide');
+      }, function (err) {
+        s.onSave = false;
+        $scope.onSaveSuccessSource = false;
+        $scope.onSaveErrorSource = true;
+        $('#newSourceModal').modal('hide');
+        console.log(err);
+      });
+    };
+    $scope.deleteSource = function (s) {
+      s.onSave = true;
+      ApplicationService.deleteVideoSource(s.name).then(function (res) {
+        s.onSave = false;
+        $scope.getSourceList();
+        $('#deleteModal').modal('hide');
+      }, function (err) {
+        s.onSave = false;
+        console.log(err);
+      });
+    };
+    $scope.cancelSource = function (s) {
+      $scope.newSource = {
+        verified: false,
+        isChecking: false,
+        checked: false
+      };
+      s = $scope.newSource;
+      s.onMod = false;
+    };
+
+    $scope.checkConnection = function (s) {
+      s.isChecking = true;
+      ApplicationService.checkConnectionVideoSource(s).then(function (res) {
+        s.checked = true;
+        s.isChecking = false;
+        s.verified = true;
+      }, function (err) {
+        s.checked = true;
+        s.isChecking = false;
+        s.verified = false;
+        console.log(err);
+      });
+    };
+
+    $scope.getSourceList = function () {
+      ApplicationService.allVideoSources().then(function (res) {
+        $scope.allSources = res.data;
+        $scope.view.changeRoute = false;
+      }, function (err) {
+        $scope.view.changeRoute = false;
+        console.log(err);
+      });
+    };
+
+    $scope.getUserList = function () {
+      $scope.view.changeRoute = true;
+      UserService.list(true).then(function (res) {
+        $scope.users = res.data;
+        $scope.getSourceList();
+      }, function (err) {
+        $scope.users = [];
+        $scope.view.changeRoute = false;
+        console.log(err);
+      });
+    };
+
+    $scope.getAllProfiles = function () {
+      ProfileService.allProfiles().then(function (res) {
+        $scope.allProfiles = res.data;
+      }, function (err) {
+        console.log(err);
+      });
+    };
+
+    $scope.getUserList();
+    $scope.getAllProfiles();
+  });
