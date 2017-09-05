e5616fdaf22842c2b9e17db8752e1be79a3671e5
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2017-09-05 15:36:23 +0200

    changed external freepbx link

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -2,8 +2,8 @@ var customConfig = {
   BRAND_NAME: 'NethVoice',
   BRAND_SITE: 'http://www.nethvoice.it',
   BRAND_DOCS: 'http://nethvoice.docs.nethesis.it/it/latest/',
-  BASE_API_URL: 'https://nethpanico.nethesis.it/freepbx/rest-dev',
-  BASE_API_URL_CTI: 'https://nethpanico.nethesis.it/webrest',
+  BASE_API_URL: '/freepbx/rest',
+  BASE_API_URL_CTI: '/webrest',
   VPLAN_URL: '/freepbx/visualplan',
   OUTBOUNDS_URL: '/freepbx/admin/config.php?display=routing&view=form&id=',
   SECRET_KEY: '1234'
