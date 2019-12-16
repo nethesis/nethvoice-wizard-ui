d3e03cafd58f52bdc5e36c6f4f5e5ce55bc55e49
SebastianMB-IT <sebastian.besel@nethesis.it>
2019-12-16 18:35:10 +0100

    Add configurations service and add some logic

--- wizard/app/scripts/controllers/configurations.js
+++ wizard/app/scripts/controllers/configurations.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /**
  * @ngdoc function
@@ -8,169 +8,15 @@
  * Controller of the nethvoiceWizardUiApp
  */
 angular.module('nethvoiceWizardUiApp')
-  .controller('ConfigurationsCtrl', function ($scope) {
+  .controller('ConfigurationsCtrl', function ($scope, ConfigurationService, ProfileService, DeviceService) {
 
+    $scope.view.changeRoute = true
+    
+    $scope.allUsers = {}
+    $scope.allProfiles = []
+    $scope.allGroups = []
+    $scope.allDevices = []
 
-    $scope.allUsers = [{
-      "id": "15",
-      "auth": "2",
-      "authid": "b61769be-0521-1039-8778-99488d099b84",
-      "username": "testuser1",
-      "description": "",
-      "password": false,
-      "default_extension": "201",
-      "primary_group": "1000",
-      "permissions": null,
-      "fname": "",
-      "lname": "testuser1",
-      "displayname": "Test User 1",
-      "title": "",
-      "company": null,
-      "department": "",
-      "language": null,
-      "timezone": null,
-      "dateformat": null,
-      "timeformat": null,
-      "datetimeformat": null,
-      "email": "testuser1@vm3.local",
-      "cell": "",
-      "work": "",
-      "home": "",
-      "fax": "",
-      "dn": "Test User 1",
-      "devices": [{
-        "id": "187",
-        "user_id": "15",
-        "mac": "00:15:65:11:5B:B5",
-        "vendor": "Yealink\/Dreamwave",
-        "model": "T26P",
-        "line": "1",
-        "extension": "91201",
-        "secret": "c4a9a9c0f939eb17f29a6d511f9da042",
-        "web_user": null,
-        "web_password": null,
-        "type": "physical"
-      }, {
-        "id": "198",
-        "user_id": "15",
-        "mac": null,
-        "vendor": null,
-        "model": null,
-        "line": null,
-        "extension": "201",
-        "secret": "7c0c1e11776b11c3ec57e8c35b305915",
-        "web_user": null,
-        "web_password": null,
-        "type": "webrtc"
-      }],
-      "profile": "3"
-    }, {
-      "id": "11",
-      "auth": "2",
-      "authid": "ba67e868-0521-1039-8781-99488d099b84",
-      "username": "testuser10",
-      "description": "",
-      "password": false,
-      "default_extension": "210",
-      "primary_group": "1000",
-      "permissions": null,
-      "fname": "",
-      "lname": "testuser10",
-      "displayname": "Test User 10",
-      "title": "",
-      "company": null,
-      "department": "",
-      "language": null,
-      "timezone": null,
-      "dateformat": null,
-      "timeformat": null,
-      "datetimeformat": null,
-      "email": "testuser10@vm3.local",
-      "cell": "",
-      "work": "",
-      "home": "",
-      "fax": "",
-      "dn": "Test User 10",
-      "devices": [{
-        "id": "19",
-        "user_id": "11",
-        "mac": null,
-        "vendor": null,
-        "model": null,
-        "line": null,
-        "extension": "210",
-        "secret": "5cabc2887653a3a28900e5287f43581f",
-        "web_user": null,
-        "web_password": null,
-        "type": "webrtc"
-      }, {
-        "id": "20",
-        "user_id": "11",
-        "mac": null,
-        "vendor": null,
-        "model": null,
-        "line": null,
-        "extension": "91210",
-        "secret": "c7c7ee1d76d0f98bce664c0b0b256bbb",
-        "web_user": null,
-        "web_password": null,
-        "type": "webrtc_mobile"
-      }],
-      "profile": "3"
-    }, {
-      "id": "50",
-      "auth": "2",
-      "authid": "5f143394-0522-1039-87d1-99488d099b84",
-      "username": "testuser100",
-      "description": "",
-      "password": false,
-      "default_extension": "300",
-      "primary_group": "1000",
-      "permissions": null,
-      "fname": "",
-      "lname": "testuser100",
-      "displayname": "Test User 100",
-      "title": "",
-      "company": null,
-      "department": "",
-      "language": null,
-      "timezone": null,
-      "dateformat": null,
-      "timeformat": null,
-      "datetimeformat": null,
-      "email": "testuser100@vm3.local",
-      "cell": "",
-      "work": "",
-      "home": "",
-      "fax": "",
-      "dn": "Test User 100",
-      "devices": [{
-        "id": "179",
-        "user_id": "50",
-        "mac": null,
-        "vendor": null,
-        "model": null,
-        "line": null,
-        "extension": "300",
-        "secret": "d658d75c759ebb4c0ceb179fdddacde9",
-        "web_user": null,
-        "web_password": null,
-        "type": "webrtc"
-      }, {
-        "id": "180",
-        "user_id": "50",
-        "mac": null,
-        "vendor": null,
-        "model": null,
-        "line": null,
-        "extension": "91300",
-        "secret": "206af30b19adf6070ee0b023d630f677",
-        "web_user": null,
-        "web_password": null,
-        "type": "webrtc_mobile"
-      }],
-      "profile": "3"
-    }]
 
     $scope.loadingUser = {}
     $scope.loadingModel = {}
@@ -201,16 +47,324 @@ angular.module('nethvoiceWizardUiApp')
           $scope.loadingModel[name] = false
           $scope.$apply()
         }, 1000)
-
-        console.log($scope.currentModel);
-        
+        console.log($scope.currentModel)
       }, function (err) {
         console.log(err)
       })
     }
 
     $scope.openDevices = function () {
-      console.log("Open association modal");
+      console.log("Open association modal")
     }
 
-  });
+    $scope.availableUserFilters = ['all', 'configured', 'unconfigured']
+    $scope.availableUserFiltersNumbers = ['lname', 'default_extension']
+    $scope.currentUserFilter = $scope.availableUserFilters[0]
+    $scope.currentUserFilterNumbers = $scope.availableUserFiltersNumbers[0]
+
+
+    // $scope.maxExtensionReached = false
+    // $scope.newDevice = {}
+
+    // $scope.cancelError = function () {
+    //   $scope.maxExtensionReached = false
+    // }
+
+    var getAllProfiles = function () {
+      ProfileService.allProfiles().then(function (res) {
+        $scope.allProfiles = res.data
+      }, function (err) {
+        console.log(err)
+      })
+    }
+
+    var getAllGroups = function () {
+      ProfileService.allGroups().then(function (res) {
+        $scope.allGroups = res.data
+      }, function (err) {
+        console.log(err)
+      })
+    }
+
+    var getAllUser = function () {
+      ConfigurationService.list(false).then(function (res) {
+        $scope.allUsers = res.data
+        $scope.view.changeRoute = false
+        getDeviceList()
+        // var index = 0
+        // for (var u in $scope.allUsers) {
+        //   if ($scope.allUsers[u].default_extension !== 'none') {
+        //     index = u
+        //     break
+        //   } else {
+        //     continue
+        //   }
+        // }
+        // $scope.selectUser($scope.currentUserIndex || $scope.allUsers[index], true) 
+        // if ($scope.mode.isLdap && UtilService.isEmpty($scope.allUsers)) {
+        //   $scope.wizard.nextState = false 
+        // }
+      }, function (err) {
+        console.log(err)
+      })
+    }
+
+    var getDeviceList = function () {
+      DeviceService.phoneList().then(function (res) {
+        $scope.allDevices = res.data
+      }, function (err) {
+        console.log(err)
+      })
+    }
+
+    $scope.getNameFromExtension = function (main) {
+      if ($scope.allUsers.filter) {
+        var returned = $scope.allUsers.filter(function (obj) {
+          if (obj.default_extension == main) {
+            return obj
+          }
+        })[0]
+        return returned && returned.displayname ? returned.displayname : ''
+      }
+    }
+
+    $scope.resetDeviceSearch = function () {
+      $scope.searchDeviceUserString = ''
+    }
+
+    $scope.selectUser = function (user, first) {
+      if (!first) {
+        if (user.devices.length > 0) {
+          for (var d in user.devices) {
+            if (user.devices[d].type === 'physical' || user.devices[d].type === 'temporaryphysical') {
+              $scope.searchDeviceUserString = user.username
+              break
+            } else {
+              $scope.searchDeviceUserString = ''
+            }
+          }
+        } else {
+          $scope.searchDeviceUserString = ''
+        }
+      }
+      if (user.default_extension !== 'none') {
+        $scope.currentUserIndex = user
+        $scope.currentUser = $scope.allUsers.filter(function (obj) {
+          if (obj.id == user.id) {
+            return obj
+          }
+        })[0]
+        ProfileService.getUserGroup($scope.currentUser.id).then(function (res) {
+          $scope.currentUser.groups = res.data
+        }, function (err) {
+          if (err.status != 404) {
+            console.log(err)
+          }
+        })
+        UserService.getMobileExtension($scope.currentUser.username).then(function (res) {
+          $scope.currentUser.mobile = res.data
+        }, function (err) {
+          if (err.status != 404) {
+            console.log(err)
+          }
+        })
+        UserService.getVoiceMail($scope.currentUser.default_extension).then(function (res) {
+          if (res.data && res.data.mailbox) {
+            $scope.currentUser.voiceMailState = true
+          } else {
+            $scope.currentUser.voiceMailState = false
+          }
+        }, function (err) {
+          if (err.status != 404) {
+            console.log(err)
+          }
+          $scope.currentUser.voiceMailState = false
+        })
+        UserService.getWebRTCExtension($scope.currentUser.default_extension).then(function (res) {
+          $scope.currentUser.webRtcState = true
+        }, function (err) {
+          console.log(err)
+          $scope.currentUser.webRtcState = false
+        })
+      }
+    }
+
+    $scope.configureAndRebootPhone = function (device) {
+      device.setPhysicalInAction = true
+      DeviceService.generateDeviceConfig({
+        mac: device.mac,
+      }).then(function (res) {
+        DeviceService.rebootPhone({
+          mac: device.mac,
+          ip: device.ipv4
+        }).then(function (res1) {
+          console.log(res1)
+          device.setPhysicalInAction = false
+          device.inError = false
+        }, function (err1) {
+          console.log(err1)
+          device.setPhysicalInAction = false
+          device.inError = true
+        })
+      }, function (err) {
+        console.log(err)
+        device.setPhysicalInAction = false
+        device.inError = true
+      })
+    }
+
+    $scope.bulkPhonesConfiguration = function (str) {
+      var devices = $filter('filter')($scope.allDevices, str)
+      for (var d in devices) {
+        if ($scope.isConfigured(devices[d])) {
+          $scope.configureAndRebootPhone(devices[d])
+          $('#bulkModal').modal('hide')
+        }
+      }
+    }
+
+    $scope.isConfigured = function (device) {
+      for (var l in device.lines) {
+        var line = device.lines[l]
+        if (line.extension) {
+          return true
+        }
+      }
+    }
+
+    $scope.setPhysicalExtension = function (user, device, line) {
+      device.setPhysicalInAction = true
+      UserService.createPhysicalExtension({
+        mainextension: user.default_extension,
+        mac: device.mac || null,
+        model: device.model || null,
+        line: line || null,
+        web_user: device.web_user || 'admin',
+        web_password: device.web_password || 'admin'
+      }).then(function (res) {
+        device.setPhysicalInAction = false
+        device.web_password = ''
+        device.web_user = ''
+        getAllUser(false)
+      }, function (err) {
+        device.setPhysicalInAction = false
+        console.log(err)
+        if (err.data.status == "There aren't available extension numbers") {
+          $scope.maxExtensionReached = true
+        }
+      })
+    }
+
+    $scope.deletePhysicalExtension = function (device, extension) {
+      device.setPhysicalInAction = true
+      UserService.deletePhysicalExtension(extension).then(function (res) {
+        device.setPhysicalInAction = false
+        getAllUser(false)
+        console.log(res)
+      }, function (err) {
+        device.setPhysicalInAction = false
+        console.log(err)
+      })
+    }
+
+    $scope.setMobileExtension = function (user) {
+      $scope.currentUser.setMobileInAction = true
+      UserService.createMobileExtension({
+        username: user.username,
+        mobile: user.mobile
+      }).then(function (res) {
+        $scope.currentUser.setMobileInAction = false
+      }, function (err) {
+        console.log(err)
+        $scope.currentUser.setMobileInAction = false
+      })
+    }
+
+    $scope.setVoiceMail = function () {
+      $scope.currentUser.setVoiceMailInAction = true
+      UserService.createVoiceMail({
+        extension: $scope.currentUser.default_extension,
+        state: $scope.currentUser.voiceMailState ? 'yes' : 'no'
+      }).then(function (res) {
+        $scope.currentUser.setVoiceMailInAction = false
+      }, function (err) {
+        console.log(err)
+        $scope.currentUser.setVoiceMailInAction = false
+      })
+    }
+
+    $scope.setWebRTC = function (event, state) {
+      $scope.currentUser.setWebRTCInAction = true
+      if ($scope.currentUser.webRtcState) {
+        UserService.createWebRTCExtension({
+          extension: $scope.currentUser.default_extension
+        }).then(function (res) {
+          $scope.currentUser.setWebRTCInAction = false
+          getAllUser(false)
+        }, function (err) {
+          console.log(err)
+          $scope.currentUser.setWebRTCInAction = false
+        })
+      } else {
+        UserService.deleteWebRTCExtension($scope.currentUser.default_extension).then(function (res) {
+          $scope.currentUser.setWebRTCInAction = false
+          getAllUser(false)
+        }, function (err) {
+          console.log(err)
+          $scope.currentUser.setWebRTCInAction = false
+        })
+      }
+    }
+
+    $scope.setAppMobile = function (event, state) {
+      $scope.currentUser.setAppMobileInAction = true
+      /*UserService.createVoiceMail({
+        extension: $scope.currentUser.default_extension,
+        state: state ? 'yes' : 'no'
+      }).then(function (res) {
+        $scope.currentUser.setVoiceMailInAction = false 
+      }, function (err) {
+        console.log(err) 
+        $scope.currentUser.setVoiceMailInAction = false 
+      }) */
+    }
+
+    $scope.setProfile = function () {
+      ProfileService.setUserProfile($scope.currentUser.id, {
+        profile_id: $scope.currentUser.profile
+      }).then(function (res) {
+        console.log(res)
+      }, function (err) {
+        console.log(err)
+      })
+    }
+
+    $scope.setGroup = function () {
+      ProfileService.setUserGroup($scope.currentUser.id, {
+        groups: $scope.currentUser.groups
+      }).then(function (res) {
+        console.log(res)
+      }, function (err) {
+        console.log(err)
+      })
+    }
+
+    $scope.checkConfiguredExtensions = function (device, filter) {
+      if (filter == 'all') {
+        return true
+      }
+      var count = device.lines.length
+      for (var l in device.lines) {
+        var line = device.lines[l]
+        if (line.extension) {
+          count--
+        }
+      }
+      return filter == 'unlinked' ? count == device.lines.length : count != device.lines.length
+    }
+
+    getAllUser()
+    getAllProfiles()
+    getAllGroups()
+
+  })
---
+++ wizard/app/scripts/services/configurationservice.js
@@ -0,0 +1,123 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name nethvoiceWizardUiApp.ConfigurationService
+ * @description
+ * # ConfigurationService
+ * Service in the nethvoiceWizardUiApp.
+ */
+angular.module('nethvoiceWizardUiApp')
+  .service('ConfigurationService', function($q, RestService) {
+
+    this.list = function (filtered) {
+      return $q(function (resolve, reject) {
+        RestService.get('/users/' + filtered).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.createPhysicalExtension = function (obj) {
+      return $q(function (resolve, reject) {
+        RestService.post('/physicalextensions', obj).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.deletePhysicalExtension = function (ext) {
+      return $q(function (resolve, reject) {
+        RestService.delete('/physicalextensions/' + ext).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.getVoiceMail = function (extension) {
+      return $q(function (resolve, reject) {
+        RestService.get('/voicemails/' + extension).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.createVoiceMail = function (obj) {
+      return $q(function (resolve, reject) {
+        RestService.post('/voicemails', obj).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.getMobileExtension = function (username) {
+      return $q(function (resolve, reject) {
+        RestService.get('/mobiles/' + username).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.createMobileExtension = function (obj) {
+      return $q(function (resolve, reject) {
+        RestService.post('/mobiles', obj).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.getWebRTCExtension = function (mainextension) {
+      return $q(function (resolve, reject) {
+        RestService.get('/webrtc/' + mainextension).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.createWebRTCExtension = function (obj) {
+      return $q(function (resolve, reject) {
+        RestService.post('/webrtc', obj).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.deleteWebRTCExtension = function (mainextension) {
+      return $q(function (resolve, reject) {
+        RestService.delete('/webrtc/' + mainextension).then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+    this.retrieveFinalInfo = function () {
+      return $q(function (resolve, reject) {
+        RestService.get('/final').then(function (res) {
+          resolve(res);
+        }, function (err) {
+          reject(err);
+        });
+      });
+    };
+
+  });
