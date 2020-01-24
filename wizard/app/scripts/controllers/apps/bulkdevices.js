2d75dac032a14361211cc8cfc81f861f2c1dba0f
Andrea Leardini <andrea.leardini@nethesis.it>
2020-01-24 16:27:32 +0100

    Enhanced link device to user modal. Minor UI fixes

--- wizard/app/scripts/controllers/apps/bulkdevices.js
+++ wizard/app/scripts/controllers/apps/bulkdevices.js
@@ -155,7 +155,7 @@ angular.module('nethvoiceWizardUiApp')
 
       $timeout(function () {
         $scope.phonesHeight = 'calc(100vh - ' + ($('#phone-list')[0].getBoundingClientRect().y + 80) + 'px)';
-      }, 200);
+      }, 400);
 
       // set phone.user
       $scope.phones.forEach(function (phone) {
@@ -463,7 +463,7 @@ angular.module('nethvoiceWizardUiApp')
           // success
           $timeout(function () {
             $('#reboot-now-modal').modal('hide');
-          }, 2000);
+          }, 2500);
         }
       }, function (err) {
         console.log(err);
