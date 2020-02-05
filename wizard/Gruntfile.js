9d2c14bfeecfc463ee307ebd795092587573d250
SebastianMB-IT <sebastian.besel@nethesis.it>
2020-02-05 17:48:14 +0100

    Add the es6 sintax for the ui and fix the broken build

--- wizard/Gruntfile.js
+++ wizard/Gruntfile.js
@@ -16,7 +16,8 @@ module.exports = function (grunt) {
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
-    cdnify: 'grunt-google-cdn'
+    cdnify: 'grunt-google-cdn',
+    uglify: 'grunt-contrib-uglify-es'
   });
 
   // Configurable paths for the application
