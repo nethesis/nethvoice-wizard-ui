ddd04ef2b1ad87d6150fae8a2dabf6b73e5182db
Andrea Leardini <andrea.leardini@nethesis.it>
2020-02-05 12:04:54 +0100

    Fix project build

--- wizard/app/scripts/webcomponents/scrolling-sentinel.js
+++ wizard/app/scripts/webcomponents/scrolling-sentinel.js
@@ -1,27 +1,40 @@
 "use strict"
 
-customElements.define('scrolling-sentinel',
-  class extends HTMLElement {
-    constructor() {
-      super()
+function ScrollingSentinel() {
+  return Reflect.construct(HTMLElement, [], this.constructor);
+}
 
-      function dispatch(evt) {
-        var event = new Event(evt);
-        document.dispatchEvent(event);
-      }
+ScrollingSentinel.prototype = Object.create(HTMLElement.prototype);
+ScrollingSentinel.prototype.constructor = ScrollingSentinel;
+Object.setPrototypeOf(ScrollingSentinel, HTMLElement);
+
+ScrollingSentinel.prototype.connectedCallback = function() {
+  this.innerHTML = '';
+};
+customElements.define('scrolling-sentinel', ScrollingSentinel);
+
+// customElements.define('scrolling-sentinel', //// delete
+//   class extends HTMLElement {
+//     constructor() {
+//       super()
+
+//       function dispatch(evt) {
+//         var event = new Event(evt);
+//         document.dispatchEvent(event);
+//       }
       
-      this.observer = new IntersectionObserver(entries => {
-        if (entries[0].intersectionRatio <= 0) {
-          return;
-        }
-        dispatch(entries[0].target.getAttribute("event"))
+//       this.observer = new IntersectionObserver(entries => {
+//         if (entries[0].intersectionRatio <= 0) {
+//           return;
+//         }
+//         dispatch(entries[0].target.getAttribute("event"))
 
-      });
-      this.observer.observe(this);
-    }
+//       });
+//       this.observer.observe(this);
+//     }
     
-    disconnectedCallback () {
-      this.observer.unobserve(this)
-    }
-  }
-)
+//     disconnectedCallback () {
+//       this.observer.unobserve(this)
+//     }
+//   }
+// )
