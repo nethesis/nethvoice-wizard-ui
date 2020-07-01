fb239d5204519dae27c493b98e7d755665a08910
Davide Principi <davide.principi@nethesis.it>
2020-07-01 18:04:36 +0200

    Fix response with JSON.parse

--- wizard/app/scripts/macVendors.js
+++ wizard/app/scripts/macVendors.js
@@ -2,5 +2,5 @@ var macVendors = ((function () {
   var req = new XMLHttpRequest();
   req.open("GET", "/tancredi/api/v1/macvendors", false);
   req.send();
-  return req.status == 200 && req.responseType == "json" ? req.response : {};
+  return req.status == 200 ? JSON.parse(req.response) : {};
 })());
