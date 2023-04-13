4608e8458d8225dbc51445561f0236c4c8a15e48
tonyco97 <tonyco97.tc@gmail.com>
2023-04-13 11:29:27 +0200

    Created upload file function and other UI minor fix

--- wizard/app/scripts/controllers/apps/voicemailText.js
+++ wizard/app/scripts/controllers/apps/voicemailText.js
@@ -9,9 +9,46 @@
  */
 angular
   .module("nethvoiceWizardUiApp")
-  .controller("AppsVoicemailTextCtrl", function ($rootScope, $scope, VoicemailTextService) {
-    $scope.voicemailTextEnabled = false;
-    $scope.toggleVoicemailText = function () {
-      $scope.voicemailTextEnabled == !$scope.voicemailTextEnabled;
-    };
-  });
+  .controller(
+    "AppsVoicemailTextCtrl",
+    function ($rootScope, $scope, VoicemailTextService) {
+      $scope.voicemailTextEnabled = false;
+      $scope.tempVoicemail = {};
+
+      $scope.errorVoicemailUpload = {
+        file: {
+          status: false,
+          title: "File format error",
+          content: "File must be a JSON",
+        },
+      };
+
+      $scope.toggleVoicemailText = function () {
+        $scope.voicemailTextEnabled == !$scope.voicemailTextEnabled;
+      };
+
+      $scope.googleAuthorizationUpload = function () {
+        $("#uploadInput").click();
+        $("#uploadInput").change(function (e) {
+          if (e.target.files[0].name != undefined) {
+            $scope.tempVoicemail.jsonFileName = e.target.files[0].name;
+            var reader = new FileReader();
+            reader.onload = function (ev) {
+              $scope.$apply(function () {
+                $scope.tempVoicemail.file64 = ev.target.result;
+                $scope.errorVoicemailUpload.file.status = false;
+                $("#uploadInput").val("");
+                $("#uploadInput").unbind();
+              });
+            };
+            reader.readAsDataURL(e.target.files[0]);
+            console.log("this",$scope.tempVoicemail.jsonFileName)
+          } else {
+            $scope.$apply(function () {
+              $scope.error.fileJson.status = true;
+            });
+          }
+        });
+      };
+    }
+  );
